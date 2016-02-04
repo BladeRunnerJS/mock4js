a8f5374d52a5574dec74214fef0d28e5b48d2d52
dchambers <dominic.chambers@gmail.com>
2016-02-04 14:21:11 +0000

    Add shims so tests can be run in IE8

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function(config) {
 
 		// list of files / patterns to load in the browser
 		files: [
+			'shims.js',
 			'test/**/*.js'
 		],
 
@@ -23,6 +24,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
+			'shims.js': ['browserify'],
 			'test/**/*.js': ['browserify']
 		},
 
